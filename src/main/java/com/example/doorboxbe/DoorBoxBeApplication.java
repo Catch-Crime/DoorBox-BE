7ad8bcf10f96e05cda732dd73d90250613661899
m1nzez <vivi020405@naver.com>
2025-08-21 00:00:34 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/doorboxbe/DoorBoxBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.doorboxbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorBoxBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorBoxBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doorboxbe/DoorBoxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doorboxbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorBoxBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
