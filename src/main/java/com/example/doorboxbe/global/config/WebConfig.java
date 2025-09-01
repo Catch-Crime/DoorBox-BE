63c96b8028da0450a1287bf70711ccb70c057801
m1nzez <vivi020405@naver.com>
2025-09-02 06:52:49 +0900

    :sparkles: feat: 멤버 토큰 권한 확인

---
+++ src/main/java/com/example/doorboxbe/global/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.doorboxbe.global.config;
+
+import com.example.doorboxbe.domain.auth.annotation.AuthenticatedMemberResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+// 만든 AuthenticatedMemberResolver를 사용하도록 설정
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticatedMemberResolver authenticatedMemberResolver;
+
+    public WebConfig(AuthenticatedMemberResolver authenticatedMemberResolver) {
+        this.authenticatedMemberResolver = authenticatedMemberResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedMemberResolver);
+    }
+
+}
